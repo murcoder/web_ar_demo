2de501e5033e5ad998f79ef58f05a4de45f42a49
murcoder <chris.murauer@yahoo.de>
2025-09-01 12:49:23 +0200

    * added react-three-mind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import { Canvas } from '@react-three/fiber';
-import { Experience } from './components/Experience';
 import React from 'react';
-import { createXRStore, XR } from '@react-three/xr';
-import NavBar from './html/NavBar.jsx';
 import { ARAnchor, ARView } from 'react-three-mind';
-
-const store = createXRStore();
 
 function App() {
 
@@ -13,33 +7,21 @@ function App() {
     <>
       <ARView
         imageTargets="/targets/card.mind"
-        filterMinCF={0.1}
+        filterMinCF={0.001}
         filterBeta={1000}
         missTolerance={5}
         warmupTolerance={5}
-        flipUserCamera={true}
+        flipUserCamera={false}
       >
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <ARAnchor
-          target={0}
-          onAnchorFound={() => console.log('Target found! 🎉')}
-          onAnchorLost={() => console.log('Target lost! 😢')}
-        >
-          {/*<Experience />*/}
-          <mesh position={[0, 0, 0]}>
-            <planeGeometry />
+        <ARAnchor target={0}>
+          <mesh position={[0,0,0]} rotation={[0,0,0]}>
+            <boxGeometry />
             <meshNormalMaterial />
           </mesh>
         </ARAnchor>
       </ARView>
-      {/*<NavBar />*/}
-      {/*<Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>*/}
-      {/*  <XR store={store}>*/}
-      {/*    <color attach="background" args={['#ececec']} />*/}
-      {/*    <Experience />*/}
-      {/*  </XR>*/}
-      {/*</Canvas>*/}
     </>
   );
 }
